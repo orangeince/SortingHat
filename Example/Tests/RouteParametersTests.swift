ecc8cf63ce78f1ad6c235fd0b70e073e0a28e92c
Shao <orangeince@outlook.com>
2019-03-12 13:15:58 +0800

    add: Test case of StringConvertible.

--- Example/Tests/RouteParametersTests.swift
+++ Example/Tests/RouteParametersTests.swift
@@ -91,4 +91,81 @@ class RouteParametersTests: XCTestCase {
         XCTAssert(params.uint32.value == 32)
         XCTAssert(params.uint64.value == 64)
     }
+    
+    func testParametersWithValueType() {
+        // - Normal value type of dictionary.
+        struct Parameters: ParametersDecodable {
+            let string: ValueType.String
+            let float: ValueType.Float
+            let double: ValueType.Double
+            let int: ValueType.Int
+            let int8: ValueType.Int8
+            let int16: ValueType.Int16
+            let int32: ValueType.Int32
+            let int64: ValueType.Int64
+            let uint: ValueType.UInt8
+            let uint8: ValueType.UInt8
+            let uint16: ValueType.UInt16
+            let uint32: ValueType.UInt32
+            let uint64: ValueType.UInt64
+        }
+        let params = Parameters.init(params: [
+            "string": "swift",
+            "float": 4.2,
+            "double": 4.2,
+            "int": 0,
+            "int8": 8,
+            "int16": 16,
+            "int32": 32,
+            "int64": 64,
+            "uint": 0,
+            "uint8": 8,
+            "uint16": 16,
+            "uint32": 32,
+            "uint64": 64
+        ])!
+        XCTAssert(params.string.value == "swift")
+        XCTAssert(params.float.value == 4.2)
+        XCTAssert(params.double.value == 4.2)
+        XCTAssert(params.int.value == 0)
+        XCTAssert(params.int8.value == 8)
+        XCTAssert(params.int16.value == 16)
+        XCTAssert(params.int32.value == 32)
+        XCTAssert(params.int64.value == 64)
+        XCTAssert(params.uint.value == 0)
+        XCTAssert(params.uint8.value == 8)
+        XCTAssert(params.uint16.value == 16)
+        XCTAssert(params.uint32.value == 32)
+        XCTAssert(params.uint64.value == 64)
+        
+        // - String value type of dictionary.
+        let params2 =  Parameters.init(params: [
+            "string": "swift",
+            "float": "4.2",
+            "double": "4.2",
+            "int": "0",
+            "int8": "8",
+            "int16": "16",
+            "int32": "32",
+            "int64": "64",
+            "uint": "0",
+            "uint8": "8",
+            "uint16": "16",
+            "uint32": "32",
+            "uint64": "64"
+            ])!
+        XCTAssert(params2.string.value == "swift")
+        XCTAssert(params2.float.value == 4.2)
+        XCTAssert(params2.double.value == 4.2)
+        XCTAssert(params2.int.value == 0)
+        XCTAssert(params2.int8.value == 8)
+        XCTAssert(params2.int16.value == 16)
+        XCTAssert(params2.int32.value == 32)
+        XCTAssert(params2.int64.value == 64)
+        XCTAssert(params2.uint.value == 0)
+        XCTAssert(params2.uint8.value == 8)
+        XCTAssert(params2.uint16.value == 16)
+        XCTAssert(params2.uint32.value == 32)
+        XCTAssert(params2.uint64.value == 64)
+    }
 }
