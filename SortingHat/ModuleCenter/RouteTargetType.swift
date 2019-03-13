42b06ac774bffb3730e28f83aaf9968e02ddca24
Shao <orangeince@outlook.com>
2019-03-13 22:13:03 +0800

    add: Code comments for RouteTarget.

--- SortingHat/ModuleCenter/RouteTargetType.swift
+++ SortingHat/ModuleCenter/RouteTargetType.swift
@@ -7,7 +7,32 @@
 
 import Foundation
 
+/// Target of route node.
+///
+/// Example:
+/// ```
+/// enum StoryModule: RouteTargetType {
+///     case detail
+///     case list
+///
+///     var node: RouteNodeType {
+///         switch self {
+///             case .detail: return RouteNode<DetailViewController>()
+///             case .list: return RouteNode<ListViewController>()
+///         }
+///     }
+///
+///     var parameters: [String: Any] {
+///         switch self {
+///             case .detail: return ["id": 89]
+///             case .list: return [:]
+///         }
+///     }
+/// }
+/// ```
 public protocol RouteTargetType {
+    /// Route node for target.
     var node: RouteNodeType { get }
+    /// Parameters required by route node.
     var parameters: [String: Any] { get }
 }
